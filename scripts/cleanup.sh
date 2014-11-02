1b869b34ce753458d6df4d8b5d8a8b6d82276119
Bassam Tabbara <bassam@symform.com>
2014-11-02 09:18:46 -0800

    make use of provisioners instead of doing everything in boot commands. Remove /etc/machine-id causing it to be regenerated on first boot.

---
+++ scripts/cleanup.sh
@@ -0,0 +1,2 @@
+# remove the machine id. it will be regenerated on first boot.
+sudo rm -fr /etc/machine-id
---
+++ scripts/oem.sh
@@ -0,0 +1,3 @@
+sudo mv ~/coreos-setup-environment /usr/share/oem
+sudo chmod +x /usr/share/oem/coreos-setup-environment
+sudo mv ~/cloud-config.yml /usr/share/oem
